4af8dd825a3f6b5d9735462bbbece2724d02f599
Sudharsan Sivasankaran <sudharsan@admins-MacBook-Pro-6.local>
2014-04-26 18:30:49 +0530

    changed template

--- rabbitmq_wrapper/attributes/default.rb
+++ rabbitmq_wrapper/attributes/default.rb
@@ -1,8 +1,16 @@
-
 #Change default user
 default['rabbitmq']['default_user'] = 'freshdesk'
 default['rabbitmq']['default_pass'] = 'fresh@desk'
 
+#networking
+default['rabbitmq']['web_console_ssl_port'] = "8080"  
+default['rabbitmq']['tcp_listener_custom']= "9999"
 
+# clustering
+default['rabbitmq']['cluster'] = false 
+default['rabbitmq']['cluster_disk_nodes'] = ['rabbit@rabbitmq1','rabbit@rabbitmq2']
+default['rabbitmq']['erlang_cookie'] = 'AnyAlphaNumericStringWillDo'
+default['rabbitmq']['cluster_partition_handling'] = 'ignore'
 
-default['rabbitmq']['web_console_ssl_port'] = 8080
+# see https://github.com/opscode-cookbooks/rabbitmq/blob/master/attributes/default.rb for more attributes.
+
