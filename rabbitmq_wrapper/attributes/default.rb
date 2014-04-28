98bcd52e245f57e4d8faf9d40817b76fa5e4223a
Sudharsan Sivasankaran <sudharsan@admins-MacBook-Pro-6.local>
2014-04-28 14:44:18 +0530

    updated wrapper

--- rabbitmq_wrapper/attributes/default.rb
+++ rabbitmq_wrapper/attributes/default.rb
@@ -7,8 +7,8 @@ default['rabbitmq']['web_console_ssl_port'] = "8080"
 default['rabbitmq']['tcp_listener_custom']= "9999"
 
 # clustering
-default['rabbitmq']['cluster'] = false 
-default['rabbitmq']['cluster_disk_nodes'] = ['rabbit@rabbitmq1','rabbit@rabbitmq2']
+default['rabbitmq']['cluster'] = true 
+default['rabbitmq']['cluster_disk_nodes'] = ['rabbit@rabbitmq3','rabbit@rabbitmq4']
 default['rabbitmq']['erlang_cookie'] = 'AnyAlphaNumericStringWillDo'
 default['rabbitmq']['cluster_partition_handling'] = 'ignore'
 
--- rabbitmq_wrapper/recipes/default.rb
+++ rabbitmq_wrapper/recipes/default.rb
@@ -27,3 +27,23 @@ template "#{node['rabbitmq']['config_root']}/rabbitmq.config" do
   mode 00644
   notifies :restart, "service[rabbitmq-server]"
 end
+
+rabbitmq_user "guest" do
+  action :delete
+end
+
+rabbitmq_user "freshdesk" do
+  password "freshdesk"
+  action :add
+end
+
+rabbitmq_user "freshdesk" do
+  vhost "/"
+  permissions ".* .* .*"
+  action :set_permissions
+end
+
+rabbitmq_user "freshdesk" do
+  tag "administrator"
+  action :set_tags
+end
